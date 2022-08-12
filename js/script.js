84a25a3aae2c15888d58be5c72f0da73838009cb
JuanMirandaEq <108710528+JuanMirandaEq@users.noreply.github.com>
2022-08-12 13:58:59 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+    $(document).ready(function(){
+      $("#sidebarCollapse").on('click', function(){
+        $("#sidebar").toggleClass('active');
+        //if (($("#sidebar")[0].classList.contains("active")) && ($(window).width() > 768)) {
+          //$("#prueba").css({"margin-left":"0px"})
+        //}else if (!($("#sidebar")[0].classList.contains("active")) && ($(window).width() > 768)) {
+          //$("#prueba").css({"margin-left":"250px"})
+          //}
+          //else{
+            //$("#prueba").css({"margin-left":"0px"})
+          //}
+      })
+    })
---
+++ src/app.js
@@ -0,0 +1,28 @@
+(function () {
+
+angular.module('RoutingApp',['ui.router']);
+
+angular.module('RoutingApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to tab 1 if no other URL matches
+  $urlRouterProvider.otherwise('/home');
+
+  // Set up UI states
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'src/home.html'
+    })
+
+    .state('Whatispython', {
+      url: '/Whatispython',
+      templateUrl: 'src/Whatispython.html'
+    });
+}
+
+
+})();
